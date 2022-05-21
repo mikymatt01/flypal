ee8f67d8c381f753599023c9b249965cba5ec8ea
lilfra <francesco.mir24@gmail.com>
2022-05-21 16:22:15 +0200

    first commit

---
+++ FlyPal/src/main/java/model/DbConnection.java
@@ -0,0 +1,34 @@
+package model;
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
+import org.apache.tomcat.jdbc.pool.DataSource;
+
+public class DbConnection {
+	//init db costant
+	private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/flypal";
+	private static final String USERNAME = "java";
+	private static final String PASSWORD = "flypal01";
+	private static final int MAX_POOL = 100;
+	
+	private static DataSource datasource;
+	
+	public static Connection getConnection() throws SQLException{
+		if(datasource == null) {
+			PoolProperties prop = new PoolProperties();
+			prop.setUrl(DB_URL);
+			prop.setDriverClassName(DB_DRIVER);
+			prop.setUsername(USERNAME);
+			prop.setPassword(PASSWORD);
+			prop.setMaxActive(MAX_POOL);
+			prop.setInitialSize(10);
+			prop.setMinIdle(10);
+			prop.setRemoveAbandonedTimeout(600);
+			prop.setRemoveAbandoned(true);
+			datasource = new DataSource();
+			datasource.setPoolProperties(prop);
+		}
+		return datasource.getConnection();
+	}
+}
---
+++ FlyPal/src/main/java/model/Utente.java
@@ -0,0 +1,68 @@
+package model;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.math.BigInteger;
+
+public class Utente {
+	private String username,email,password,salt;
+	private int admin;
+	
+	public Utente(String user, String email, int adm) {
+		username = user;
+		this.email = email;
+		admin = adm;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getHashPassword() {
+		return password;
+	}
+
+	public void setHashPassword(String password) {
+		// generate salt
+		SecureRandom random = new SecureRandom();
+		byte[] salt = new byte[16];
+		random.nextBytes(salt);
+		this.salt = String.format("%040x", new BigInteger(1, salt));
+		
+		// generate encrypted password
+		PBEKeySpec spec = new PBEKeySpec(password.toCharArray(),salt,65536,128);
+		try {
+			SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+			byte[] hashed = factory.generateSecret(spec).getEncoded();
+			this.password = String.format("%040x", new BigInteger(1, hashed));
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+			throw new RuntimeException(e);
+		} 
+	}
+
+	public int isAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(int admin) {
+		this.admin = admin;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+}
